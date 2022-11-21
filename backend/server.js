cf22e547e71194d90e07e6c994900389944eaafb
Joe <pelikata@gmail.com>
2022-11-21 11:33:31 +0100

    week10-2

--- backend/server.js
+++ backend/server.js
@@ -1,99 +1,64 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
-const mediaTypes = {
-	"html": "text/html",
-	"jpeg": "image/jpeg",
-	"jpg": "image/jpeg",
-	"png": "image/png",
-	"svg": "image/svg+xml",
-	"json": "application/json",
-	"js": "text/javascript",
-	"css": "text/css",
-	"csv": "text/csv",
-	"mp3": "audio/mpeg",
-	"mp4": "video/mp4",
-	"oga": "audio/ogg",
-	"ogv": "video/ogg",
-	"pdf": "application/pdf",
-	"weba": "audio/webm",
-	"webm": "video/webm",
-	"webp": "image/webp",
-	"woff": "font/woff",
-	"woff2": "font/woff2",
-	"ttf": "font/ttf",
-	"gif": "image/gif"
-};
+const express = require("express");
+const fileUpload = require("express-fileupload");
+const app = express();
+const port = 6000;
+const path = require("path");
 
-const server = http.createServer((req, res) => {
+const fs = require("fs");
+app.use(express.json());
+app.use(fileUpload());
 
-	const errorHTML = `
-		
-	<!DOCTYPE html>
-	<html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link rel="preconnect" href="https://fonts.googleapis.com"> 
-		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
-		<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@800&display=swap" rel="stylesheet">
-		<style>
-			body{
-				padding: 0; margin: 0;
-				font-family: 'Montserrat', sans-serif;
-				font-weight: 800;
-				background-color: #4343F9;
-				color: #fff;
-			}
-			#root{
-				width: 100%;
-				height: 100vh;
-				display: flex;
-				justify-content: center;
-				align-items: center;
-				font-size: 21px;
-			}
-		</style>
-		<title>Not here</title>
-	</head>
-	<body>
-		<div id="root">Rise your gaze to the sky<br/>than a bit back to the URL bar<br/>and check that link again</div>
-	</body>
-	</html>
-	
-	`;
-    
-	let filePath = path.resolve(__dirname + '/../frontend' + req.url);
-    
-	fs.access(filePath, fs.constants.R_OK, (err) => {
-	if(err){
-		res.statusCode = 404;
-		res.end(errorHTML);
-	}else{
-		if(fs.statSync(filePath).isDirectory()) {
-			filePath += '/index.html';
-		}
-		fs.readFile(filePath, "binary", (err, data) => {
-			if(err) {
-				res.statusCode = 500;
-				res.end(errorHTML);
-			} else {
-				let mediaType = mediaTypes[filePath.split('.').pop()];
-      
-				if (!mediaType) {
-					mediaType = 'text/plain';
-				}
-				res.writeHead(200, { "Content-Type": mediaType });
-				res.write(data, "binary");
-				res.end();
-			}
-		});
-	}
-	});
+app.get("/", (req, res) => {
+  res.sendFile(path.join(`${__dirname}/../frontend/index.html`));
 });
 
-server.listen(9000, "127.0.0.1", () => {
-    const addr = server.address();
-		console.log(`http://${addr.address}:${addr.port}`);
+app.get("/about", (req, res) => {
+  res.send("Hello");
+});
+
+app.use("/public", express.static(`${__dirname}/../frontend/public`));
+
+app.post("/upload", (request, response) => {
+  console.log(request.body);
+  fs.writeFile(
+    `${__dirname}/data/userdata.json`,
+    JSON.stringify(request.body, null, 4),
+    (error) => {
+      if (error) {
+        console.log(error);
+        return response.status(500).send(error);
+      } else {
+        response.status(200).send("ok");
+      }
+    }
+  );
+});
+
+app.post("/upload-image", (request, response) => {
+  if (!request.files) {
+    return response.status(400).send("No files were uploaded");
+  }
+  const picture = request.files.file;
+  const picName = request.body.fileName;
+  console.log(picName);
+
+  picture.mv(`${__dirname}/data/${picName}.jpg`, (error) => {
+    if (error) {
+      console.log(error);
+      return response.status(500).send(error);
+    } else {
+      response.status(200).send("image ok");
+    }
+  });
+});
+
+/*  app.get("/style.css", (req, res) => {
+  res.sendFile(path.join(`${__dirname}/../frontend/style.css`));
+});
+app.get("/script.js", (req, res) => {
+  res.sendFile(path.join(`${__dirname}/../frontend/script.js`));
+}); */
+
+app.listen(port, () => {
+  console.log(`server is running at: http://127.0.0.1:${port}`);
 });
--- frontend/public/script.js
+++ frontend/public/script.js
@@ -6,11 +6,21 @@ form.addEventListener("submit", function (event) {
     category: document.querySelector("#category").value,
     price: parseInt(document.querySelector("#price").value),
   };
-  fetch("/product", {
+
+  const formData = new FormData();
+  formData.append("fileName", document.querySelector("#file-name").value);
+  formData.append("file", document.querySelector("#file").files[0]);
+
+  fetch("/upload", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(product),
+  });
+
+  fetch("/upload-image", {
+    method: "POST",
+    body: formData,
   });
 });
